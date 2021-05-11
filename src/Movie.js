5a4f4e7e531691127f273c5a97fc7bc7b0a154f6
정재영 <jjy600901@gmail.com>
2021-05-11 21:39:50 +0900

    Chapter 07 Finish

--- src/Movie.js
+++ src/Movie.js
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div class="movie__data">
-                <h3 class="movie__title" style={{ backgroundColor: 'red' }}>{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => { // map 함수의 두 번째 인자가 index 라고..
+                        return <li key={index} className="movie__genre">{genre}</li>; // 원소가 여러개라 여기서도 key 안해주면 워닝뜸..
+                    })}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
@@ -20,6 +25,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
