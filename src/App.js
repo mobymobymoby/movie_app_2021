50c35c741f1f726acddf693e25099a904a0b7c28
정재영 <jjy600901@gmail.com>
2021-05-11 14:20:22 +0900

    Chapter3 Finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+function Food(props) {
+  let { fav } = props;
+  return <h3>I like { fav }</h3>;
+}
+
 function App() {
-  return <div>Hello!!!!!!</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" test={true} />
+      <Food fav="돈가스" />
+      <Food fav="라면" />
+    </div>
+  );
 }
 
 export default App;
